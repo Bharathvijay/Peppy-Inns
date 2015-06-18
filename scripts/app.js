8bbd9e7aa37964bdf47b9039a19891f7cabf2188
Bharathvijay <bharathvijay@live.com>
2015-06-18 14:42:09 +0530

    Basics done

--- scripts/app.js
+++ scripts/app.js
@@ -11,4 +11,46 @@ $(document).ready(function() {
  
         e.preventDefault();
     });
+    
+    $('.arrow-next').click(function(){
+
+		console.log("I am being clicked");
+		var currentSlide = $('.active-slide');
+		var nextSlide = currentSlide.next();
+
+		var currentDot = $('.active-dot');
+		var nextDot = currentDot.next();
+
+		if(nextSlide.length === 0){
+			nextSlide = $('.slide').first();
+			nextDot = $('.dot').first();
+		}
+
+		currentSlide.fadeOut(600).removeClass('active-slide');
+		nextSlide.fadeIn(600).addClass('active-slide');
+
+		currentDot.removeClass('active-dot');
+		nextDot.addClass('active-dot');
+	});
+
+	$('.arrow-prev').click(function(){
+
+		console.log("I am being clicked");
+		var currentSlide = $('.active-slide');
+		var prevSlide = currentSlide.prev();
+
+		var currentDot = $('.active-dot');
+		var prevDot = currentDot.prev();
+
+		if(prevSlide.length === 0){
+			prevSlide = $('.slide').last();
+			prevDot = $('.dot').last();
+		}
+
+		currentSlide.fadeOut(600).removeClass('active-slide');
+		prevSlide.fadeIn(600).addClass('active-slide');
+
+		currentDot.removeClass('active-dot');
+		prevDot.addClass('active-dot');
+	});
 });
